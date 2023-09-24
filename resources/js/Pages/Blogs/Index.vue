04f5ffb2e071674918f018d56b11da80cb066894
Soung Oo Lwin <soungoolwin275@gmail.com>
2023-09-24 21:42:41 +0700

    Blog Card Layout Fix

--- resources/js/Pages/Blogs/Index.vue
+++ resources/js/Pages/Blogs/Index.vue
@@ -27,9 +27,13 @@ const truncatedContent = (content) => {
             <div class="section-title">
                 <h2><span>Blogs</span></h2>
             </div>
-            <div class="card-columns listfeaturedtag">
+            <div class="row listfeaturedtag">
                 <!-- begin post -->
-                <div class="card" v-for="blog in blogs.data" :key="blog.id">
+                <div
+                    class="card col-lg-6 col-md-12"
+                    v-for="blog in blogs.data"
+                    :key="blog.id"
+                >
                     <div class="row">
                         <div class="col-md-5 wrapthumbnail">
                             <a :href="'blogs/' + blog.slug">
@@ -83,20 +87,20 @@ const truncatedContent = (content) => {
                                             }}</span>
                                         </span>
                                         <!-- <span class="post-read-more"
-                                            ><a
-                                                href="post.html"
-                                                title="Read Story"
-                                                ><svg
-                                                    class="svgIcon-use"
-                                                    width="25"
-                                                    height="25"
-                                                    viewbox="0 0 25 25"
-                                                >
-                                                    <path
-                                                        d="M19 6c0-1.1-.9-2-2-2H8c-1.1 0-2 .9-2 2v14.66h.012c.01.103.045.204.12.285a.5.5 0 0 0 .706.03L12.5 16.85l5.662 4.126a.508.508 0 0 0 .708-.03.5.5 0 0 0 .118-.285H19V6zm-6.838 9.97L7 19.636V6c0-.55.45-1 1-1h9c.55 0 1 .45 1 1v13.637l-5.162-3.668a.49.49 0 0 0-.676 0z"
-                                                        fill-rule="evenodd"
-                                                    ></path></svg></a
-                                        ></span> -->
+                                                ><a
+                                                    href="post.html"
+                                                    title="Read Story"
+                                                    ><svg
+                                                        class="svgIcon-use"
+                                                        width="25"
+                                                        height="25"
+                                                        viewbox="0 0 25 25"
+                                                    >
+                                                        <path
+                                                            d="M19 6c0-1.1-.9-2-2-2H8c-1.1 0-2 .9-2 2v14.66h.012c.01.103.045.204.12.285a.5.5 0 0 0 .706.03L12.5 16.85l5.662 4.126a.508.508 0 0 0 .708-.03.5.5 0 0 0 .118-.285H19V6zm-6.838 9.97L7 19.636V6c0-.55.45-1 1-1h9c.55 0 1 .45 1 1v13.637l-5.162-3.668a.49.49 0 0 0-.676 0z"
+                                                            fill-rule="evenodd"
+                                                        ></path></svg></a
+                                            ></span> -->
                                     </div>
                                 </div>
                             </div>
@@ -105,6 +109,7 @@ const truncatedContent = (content) => {
                 </div>
                 <!-- end post -->
             </div>
+
             <Pagination :pagination="blogs.meta" />
         </section>
         <!-- End Blogs -->
